39ce69d2dd151f6df7c74adf4f49dacb7917a830
Deivid Alberto Guerra Carpio <dguerrac7@miumg.edu.gt>
2026-06-03 20:52:15 -0600

    Solucion de problema 4

--- arboles/src/main/java/umg/edu/progra/arboles/ArbolBinarioBusqueda.java
+++ arboles/src/main/java/umg/edu/progra/arboles/ArbolBinarioBusqueda.java
@@ -261,6 +261,33 @@ public class ArbolBinarioBusqueda {
                 && esBSTValidoRecursivo(nodo.derecho, nodo.dato, maximo);
     }
     
+    //Solucion 4
+    
+    public int ancestroComunMasBajo(int a, int b) {
+
+        if (!contiene(a) || !contiene(b)) {
+            throw new IllegalArgumentException(
+                    "Ambos valores deben existir en el arbol");
+        }
+
+        return ancestroComunMasBajoRecursivo(raiz, a, b);
+    }
+
+    private int ancestroComunMasBajoRecursivo(Nodo nodo, int a, int b) {
+
+        if (a < nodo.dato && b < nodo.dato) {
+            return ancestroComunMasBajoRecursivo(
+                    nodo.izquierdo, a, b);
+        }
+
+        if (a > nodo.dato && b > nodo.dato) {
+            return ancestroComunMasBajoRecursivo(
+                    nodo.derecho, a, b);
+        }
+
+        return nodo.dato;
+    }
+    
     // ============================================================
     // RECORRIDOS DEL ARBOL
     // ============================================================
