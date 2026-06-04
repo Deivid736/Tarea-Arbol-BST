eeb346f9c5d1cd85f6f968316edaa2b71dafe8f1
Deivid Alberto Guerra Carpio <dguerrac7@miumg.edu.gt>
2026-06-03 20:24:09 -0600

    Solucion de problema 3

--- arboles/src/main/java/umg/edu/progra/arboles/ArbolBinarioBusqueda.java
+++ arboles/src/main/java/umg/edu/progra/arboles/ArbolBinarioBusqueda.java
@@ -238,6 +238,29 @@ public class ArbolBinarioBusqueda {
                 && esBalanceadoRecursivo(nodo.derecho);
     }
     
+    //Solucion 3
+    
+    public boolean esBSTValido() {
+        return esBSTValidoRecursivo(raiz, null, null);
+    }
+
+    private boolean esBSTValidoRecursivo(Nodo nodo, Integer minimo, Integer maximo) {
+        if (nodo == null) {
+            return true;
+        }
+
+        if (minimo != null && nodo.dato <= minimo) {
+            return false;
+        }
+
+        if (maximo != null && nodo.dato >= maximo) {
+            return false;
+        }
+
+        return esBSTValidoRecursivo(nodo.izquierdo, minimo, nodo.dato)
+                && esBSTValidoRecursivo(nodo.derecho, nodo.dato, maximo);
+    }
+    
     // ============================================================
     // RECORRIDOS DEL ARBOL
     // ============================================================
